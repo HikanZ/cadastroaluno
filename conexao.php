d8f1a10c17e20007c21c258a95788013de82e183
HikanZ <hitoshi.kanashiro@gmail.com>
2019-07-31 23:28:15 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
-$hostname = "localhost";
-$user = "root";
-$password = "";
-$database = "cadastroaluno";
+$hostname = "us-cdbr-iron-east-02.cleardb.net";
+$user = "bc0bb534f87930";
+$password = "97b1e390";
+$database = "heroku_0a4306c08337c3d";
 
 $conexao = mysqli_connect($hostname, $user, $password, $database);
 
